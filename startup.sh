974b450e56ee32d8176579178142beb3da6c0006
lifeandsong <lifeandsong@gmail.com>
2024-04-28 17:10:06 +0900

    실행 후 comment 수정

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 
 nohup java -jar -javaagent:/home/swmaestro/scouter/scouter.agent.jar -Dscouter.config=/home/swmaestro/scouter/scouter.dev.conf -Dspring.profiles.active=dev /home/swmaestro/SpringBoot3GradleTemplate.jar 1> /dev/null 2>&1 &
 
-echo SpringBoot3GradleTemplate has been started !
+echo SpringBoot3GradleTemplate4MyBatis has been started !
